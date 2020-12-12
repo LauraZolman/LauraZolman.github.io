e23684ed3f99029fcc7ee84f75d12204d92500f8
Jessime Kirk <jessime.kirk@gmail.com>
2020-12-12 12:21:08 -0700

    Give this a shot

--- scripts/update_from_writing_portfolio.py
+++ scripts/update_from_writing_portfolio.py
@@ -2,33 +2,65 @@ from pathlib import Path
 
 import pandas as pd
 
-def delete_all_current_post_files():
-    _posts = Path(__file__).resolve().parent.parent / '_posts'
-    for post in _posts.iterdir():
-        post.unlink()
+
+TSV_PATH = '/Users/jessime.kirk/Downloads/Writing Portfolio - Sheet1.tsv'
 
 
-def make_tags(row):
-    base = '  - {}\n'
+def clear_files_from_folder(folder):
+    folder_path = Path(__file__).resolve().parent.parent / folder
+    for path in folder_path.iterdir():
+        if path.is_file():
+            path.unlink()
+
+
+def get_tags(row):
     tags = row['Tags'].split(', ')
     tags += row['Editorial/Marketing'].split(', ')
     tags += [row['Type of Edit'], row['Writer/Editor'], row['Favorites']]
-    tags = ''.join(base.format(t) for t in tags if not pd.isnull(t))
+    tags = [t for t in tags if not pd.isnull(t)]
     return tags
+
+
+def make_tags_string(tags):
+    base = '  - {}\n'
+    tags = ''.join(base.format(t) for t in tags)
+    return tags
+
 
 def make_subtitle(sub):
     return '> ' + sub if not pd.isnull(sub) else ''
 
 
-delete_all_current_post_files()
-df = pd.read_csv('/Users/jessime.kirk/Downloads/Writing Portfolio - Sheet1.tsv', sep='\t')
-template = Path('/Users/jessime.kirk/Code/me/laura_website/scripts/template.md').read_text()
-for i, row in df.iterrows():
-    if pd.isnull(row['Date']): continue
-    d = {'title': row['Title'].replace(':', '-'),
-         'subtitle': make_subtitle(row['Subtitle']),
-         'magazine': row['Magazine'],
-         'tags': make_tags(row),
-         'link': row['Link']}
-    outpath = '_posts/{}-{}.md'.format(row['Date'], row['Title'].replace(' ', '_').replace('/', '_'))
-    Path(outpath).write_text(template.format(**d))
+def generate_tag_pages(tags):
+    clear_files_from_folder('_pages/tags')
+    tags.remove('Favorite')  # This page is special; doesn't need regenerating
+    template_path = 'scripts/tag_template.md'
+    template = Path(template_path).read_text()
+    for tag in tags:
+        outpath = '_pages/tags/{}-archive.md'.format(tag)
+        Path(outpath).write_text(template.format(tag=tag, tag_lower=tag.lower()))
+
+def run():
+    clear_files_from_folder('_posts')
+    df = pd.read_csv(TSV_PATH, sep='\t')
+    template_path = 'scripts/template.md'
+    template = Path(template_path).read_text()
+    all_tags = set()
+    for i, row in df.iterrows():
+        if pd.isnull(row['Date']): continue
+        tags = get_tags(row)
+        d = {'title': row['Title'].replace(':', '-'),
+             'subtitle': make_subtitle(row['Subtitle']),
+             'magazine': row['Magazine'],
+             'tags': make_tags_string(tags),
+             'link': row['Link']}
+        all_tags.update(tags)
+        outpath = '_posts/{}-{}.md'.format(
+            row['Date'],
+            row['Title'].replace(' ', '_').replace('/', '_'))
+        Path(outpath).write_text(template.format(**d))
+    generate_tag_pages(all_tags)
+
+
+if __name__ == '__main__':
+    run()
